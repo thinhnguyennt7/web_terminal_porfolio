91b938b878485caa3856409a0af4c5e143f9f2ba
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2018-07-05 22:03:13 -0400

    getting start with web terminal

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { TerminalComponent } from './terminal/terminal.component';
+import { ConsoleComponent } from './console/console.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TerminalComponent,
+    ConsoleComponent
+  ],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
