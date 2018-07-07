e856080ac3871b5f9b99f75b09ef27f84242cbab
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2018-07-07 13:32:18 -0400

    add terminal interface

---
+++ src/app/terminal/terminal.ts
@@ -0,0 +1,12 @@
+export interface TerminalInterface {
+    ifFail: boolean;
+	response: any;
+	Resume: string;
+	AboutMe: string[];
+	Projects: string[];
+	Welcome: string;
+	Experience: string;
+	folder: any[];
+	mainPath: string;
+	currentDir: string;
+}
