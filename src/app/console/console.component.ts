ca66a973608e7591ac08ea8223a6035ec1eda238
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2018-07-07 13:32:40 -0400

    update terminal

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -21,8 +21,8 @@ export class ConsoleComponent implements OnInit {
     '<p style="font-size: 2em">Thinh N Nguyen</p><br/>' +
     'Hi! I am software engineer with background in web development and mobile application. I am currently junior student at Georgia Insitute of Technology, majoring in Computer Science and specializing in threads devices and info internetworks.</a><br/><br/>' +
     'I am really passionate about computer science and technology and want to contribute myself to this great technology age!</a><br/><br/>' + 
-    'For developers, please press ' + '<span ' + this.green + '>~</span>' + ' on this page to use the terminal to navigate this website.<br/><br/><br/>' +
-    '<span ' + this.green + '>thinhnguyen@ubuntu</span>:<span ' + this.red + '>/</span><span ' + this.blue + '>~</span> <br/><br/>';
+    'For developers, please press ' + '<span ' + this.green + '>~</span>' + ' on this page to use the terminal to navigate this website.<br/><br/>' +
+    '<span ' + this.green + '>thinhnguyen@ubuntu</span>:<span ' + this.red + '>/</span><span ' + this.blue + '>~</span> ^*_*^___^*_*^<br/><br/>';
     
     this.test = '<strong><ins>Hobbies</ins></strong>: I enjoy programming, math, soccer, travelling, and to explore a new thing around the world<br/><br/>'
     '<strong><ins>Technical Languagues</ins></strong>: Java; Swift; Python; Javascript; Typescipt, Shell<br/><br/>' +
--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,21 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TerminalService } from 'primeng/components/terminal/terminalservice';
+import { Subscription } from 'rxjs';
+import { TerminalInterface } from './terminal';
 
 @Component({
-  selector: 'app-terminal',
-  templateUrl: './terminal.component.html',
-  // styleUrls: ['./terminal.component.css'],
-  providers: [TerminalService]
+	selector: 'app-terminal',
+	templateUrl: './terminal.component.html',
+	styleUrls: ['./terminal.component.css'],
+	providers: [TerminalService]
 })
-export class TerminalComponent implements OnInit {
+export class TerminalComponent implements OnInit, OnDestroy {
+	subscription: Subscription;
+	ifFail: boolean;
+	response: any;
+	Resume: string;
+	AboutMe: string[] = ['Thinh_Nguyen-Resume.pdf', 'contact.txt', 'resume.txt', 'summary.txt'];
+	Projects: string[];
+	Welcome: string;
+	Experience: string;
+	folder: any[] = ['AboutMe', 'Projects', 'Welcome.txt'];
+	mainPath: string = 'thinhnguyen@ubuntu:/~ ';
+	currentDir: string = 'thinhnguyen@ubuntu/';
 
-  constructor(private terminalService: TerminalService) {
-    this.terminalService.commandHandler.subscribe(command => {
-        
-        let response = (command === 'date') ? new Date().toDateString() : 'Unknown command: ' + command;
-        this.terminalService.sendResponse(response);
-    });
-}
+	constructor(private terminalService: TerminalService) {
+		this.terminalService.commandHandler.subscribe(command => {
+				
+				console.log("Current using command: ", command);
+				if (command.includes('cd ')) {
+					let path = command.substring(3, command.length).toUpperCase();
+					console.log("The File name: ", path);
+					for (var element in this.folder) {
+						console.log(this.folder[element]);
+						if (path === this.folder[element].toUpperCase()) {
+							this.mainPath = 'thinhnguyen@ubuntu:/~' + this.folder[element] + '/';
+							console.log(this.mainPath);
+							break;
+						} else {
+							 console.log(command + ": No such directory");
+							 break;
+						}
+					}
+				}
+				
+				switch (command) {
+					case 'date': {
+						this.response = new Date().toDateString();
+						break;
+					}
+					case 'ls': {
+						this.response = this.folder[0] + '\n';
+						for (var ele = 1; ele < this.folder.length; ele++) {
+							console.log(this.folder[ele])
+							this.response += this.folder[ele] + '\n';
+						}
+						break;
+					}
+					case 'cd ..': {
+						
+					}
+					case 'cd': {
+						this.response = "";
+						break;
+					}
+					case 'cd ~': {
+						this.mainPath = 'thinhnguyen@ubuntu:/~ ';
+						break;
+					}
+					case 'pwd': {
+						this.response = this.currentDir;
+						break;
+					}
+					default: {
+						this.response = 'Command not found: ' + command;
+						break;
+					}
+				}
 
-  ngOnInit() {}
+				// Send respone to console
+				this.terminalService.sendResponse(this.response);
+		});
+	}
+
+	ngOnDestroy() {
+		if(this.subscription) {
+			this.subscription.unsubscribe();
+		}
+	}
+
+	ngOnInit() {}
 }
